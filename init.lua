d421748367928c6f33be9784d09192e430694fa5
Vik <v.boos@posteo.de>
2024-10-21 20:46:32 +0200

    Remap yank line and visually selected text

--- init.lua
+++ init.lua
@@ -189,6 +189,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.api.nvim_set_keymap('c', '<C-j>', '<Down>', { noremap = true })
 vim.api.nvim_set_keymap('c', '<C-k>', '<Up>', { noremap = true })
 
+-- Use leader+y to copy to clipboard
+vim.api.nvim_set_keymap('n', '<leader>y', '"+yy', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
